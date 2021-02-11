9cb7f6792d9e2a2376117b52270ae94b3705bd94
Maddoxk <58894216+Maddoxk@users.noreply.github.com>
2021-02-10 22:56:11 -0800

    Create getAverageArray.java

---
+++ arrays/getAverageArray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+/** Finds Average of all Elements in an Array
+@param Array Name
+@return Array Average
+*/
+  public static int getAverageArray(int[] x) {
+    int Sum = 0;
+    for (int i = 0; i < x.length; i++) {
+      Sum = Sum + x[i];
+    }
+    int Average = Sum / x.length;
+    return Average;
+  }
