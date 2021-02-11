972836650d8f8c67262f3c122b9e80b79209bc29
Maddoxk <58894216+Maddoxk@users.noreply.github.com>
2021-02-10 22:59:37 -0800

    Update and rename DescribeArray.java to describeArray.java

--- arrays/describeArray.java
+++ arrays/describeArray.java
@@ -4,7 +4,7 @@ import java.lang.*;
 @param Array Name
 @print Array Values
 */
-  public static int DescribeArray(int[] x) {
+  public static int describeArray(int[] x) {
     for (int i = 0; i < x.length; i++) {
       System.out.print(x[i] + " "); //I could only find a way to print and not return this, sorry.
     }
