029ee062aa2a50324d4752e7adb26f69236f9fed
Maddoxk <58894216+Maddoxk@users.noreply.github.com>
2021-02-10 22:57:37 -0800

    Create DescribeArray.java

---
+++ arrays/DescribeArray.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+/** Outputs each value in an Array
+@param Array Name
+@print Array Values
+*/
+  public static int DescribeArray(int[] x) {
+    for (int i = 0; i < x.length; i++) {
+      System.out.print(x[i] + " "); //I could only find a way to print and not return this, sorry.
+    }
+    return 0;
+  }
