13df415a5c6443364f1b3d0d4b2d679861c0ee0e
Maddoxk <58894216+Maddoxk@users.noreply.github.com>
2021-02-10 23:12:20 -0800

    Update searchArray.java

--- arrays/searchArray.java
+++ arrays/searchArray.java
@@ -1,6 +1,15 @@
 import java.lang.*;
 
-/** Finds an element in an Array
+/* Usage:
+isFound = searchArray(numberArray, searchNumber);
+    if (isFound == true) {
+      System.out.println(searchNumber + " is in the array.");
+    } else {
+      System.out.println(searchNumber + " is NOT in the array.");
+    }
+*/
+
+/** Finds an element in an Array (works on all array datatypes)
 @param Array Name
 @param Element to search for
 @return Array Average
@@ -14,3 +23,10 @@ import java.lang.*;
     }
     return isFound;
   }
+
+
+
+
+
+
+
