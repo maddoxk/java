eb1f2777d445e15b2b5b52083e9d53808343e26d
Maddoxk <58894216+Maddoxk@users.noreply.github.com>
2021-05-24 15:32:08 -0700

    Create print2DArray.java

---
+++ arrays/print2DArray.java
@@ -0,0 +1,7 @@
+private static void printGrid(int[][] x) {
+  for (int row = 0; row < x.length; row++) {
+      for (int col = 0; col < x[row].length; col++) {
+         System.out.print(x[row][col]);
+       }
+  }
+}
