99c38e608374fa0dd98f8280d92daafefa8ae2fa
Maddoxk <58894216+Maddoxk@users.noreply.github.com>
2021-08-03 20:29:57 -0600

    Update describeArray.java

--- arrays/describeArray.java
+++ arrays/describeArray.java
@@ -4,9 +4,8 @@ import java.lang.*;
 @param Array Name
 @print Array Values
 */
-  public static int describeArray(int[] x) {
+  public static void describeArray(int[] x) {
     for (int i = 0; i < x.length; i++) {
       System.out.print(x[i] + " "); //I could only find a way to print and not return this, sorry.
     }
-    return 0;
   }
